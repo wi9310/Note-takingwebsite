5ca02280ee5079237156ac6b701923a65bcbdb3b
William Wade <williamwadeisfocused@gmail.com>
2025-07-03 10:32:36 -0400

    Removed script, added more to the backend.

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-import bodyParser from "body-parser";
 import express from "express";
+import bodyParser from "body-parser";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const app = express();
+const port = 5500;
 
-document.querySelector()
+app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  console.log(__dirname + "/public/index.html") //log path
+  res.sendFile(__dirname + "/public/index.html");
 
-app.get()
+})
+
+app.post('/submit', (req, res) => {
+  console.log(req)
+}) 
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
